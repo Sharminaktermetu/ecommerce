4fd060dd7b4046591e4223baf0253f3f29467fec
Sharminaktermetu <sharminacc1@gmail.com>
2024-07-05 12:27:46 +0600

    delete product

--- src/module/product/product.controller.ts
+++ src/module/product/product.controller.ts
@@ -1,75 +1,90 @@
 import { ProductServices } from "./product.service";
 import { Request, Response } from "express";
-const createProduct= async (req: Request, res: Response) => {
-    try {
-   
-        const  productData = req.body;
-     
-        const result = await ProductServices.createProductIntoDB(productData);
-        res.status(200).json({
-            success: true,
-            message: "Product is created successfully",
-            data: result,
-          });
-      
-      } catch (err) {
-        console.log(err);
-      }
-  };
-  const getAllProducts = async (req: Request, res: Response) => {
-    try {
-      const result = await ProductServices.getAllProductsFromDB();
-  
-      res.status(200).json({
-        success: true,
-        message: "Products fetched successfully!",
-        data: result,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const productData = req.body;
 
-  const getSingleProduct = async (req: Request, res: Response) => {
-    try {
-      const { studentId } = req.params;
-  
-      const result = await ProductServices.getSingleProductFromDB(studentId);
-  
-      res.status(200).json({
-        success: true,
-        message: "Product fetched successfully!",
-        data: result,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-
-  const updateSingleProduct = async (req: Request, res: Response) => {
-    const { productId } = req.params;
-    const updatedData = req.body;
-
-    try {
-        const updatedProduct = await ProductServices.updateProductFromDB(productId, updatedData);
-        res.status(200).json({
-            success: true,
-            message: 'Product updated successfully!',
-            data: updatedProduct
-        });
-    } catch (error) {
-        console.error('Error updating product:', error);
-        res.status(400).json({
-            success: false,
-            
-        });
-    }
+    const result = await ProductServices.createProductIntoDB(productData);
+    res.status(200).json({
+      success: true,
+      message: "Product is created successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
-  export const ProductControllers = {
-    createProduct,
-    getAllProducts,
-    getSingleProduct,
-    updateSingleProduct
-  };
-  
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProductsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: "Products fetched successfully!",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const { productId } = req.params;
+
+    const result = await ProductServices.getSingleProductFromDB(productId);
+
+    res.status(200).json({
+      success: true,
+      message: "Product fetched successfully!",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const updateSingleProduct = async (req: Request, res: Response) => {
+  const { productId } = req.params;
+  const updatedData = req.body;
+
+  try {
+    const updatedProduct = await ProductServices.updateProductFromDB(
+      productId,
+      updatedData
+    );
+    res.status(200).json({
+      success: true,
+      message: "Product updated successfully!",
+      data: updatedProduct,
+    });
+  } catch (error) {
+    console.error("Error updating product:", error);
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
+const deleteSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const { productId } = req.params;
+
+    const result = await ProductServices.deleteProductFromDB(productId);
+
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully!",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const ProductControllers = {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateSingleProduct,
+  deleteSingleProduct
+};
--- src/module/product/product.service.ts
+++ src/module/product/product.service.ts
@@ -2,42 +2,49 @@ import mongoose from "mongoose";
 import { Product } from "./product.interface";
 import { ProductModel } from "./product.model";
 
-
 const createProductIntoDB = async (product: Product) => {
-    const result = await ProductModel.create(product);
-    return result;
-  };
-  const getAllProductsFromDB = async () => {
-    const result = await ProductModel.find();
-    return result;
-  };
-
-  const getSingleProductFromDB = async (id: string) => {
-    const result = await ProductModel.findOne({ id });
-    return result;
-  };
-  const updateProductFromDB = async (productId: string, updatedData: Partial<Product>) => {
-    // Check if the productId is a valid MongoDB ObjectID
-    if (!mongoose.Types.ObjectId.isValid(productId)) {
-        throw new Error('Invalid product ID');
-    }
-
-    // Find and update the product
-    const updatedProduct = await ProductModel.findByIdAndUpdate(
-        productId,
-        updatedData,
-        { new: true, runValidators: true }
-    );
-
-    if (!updatedProduct) {
-        throw new Error('Product not found');
-    }
-
-    return updatedProduct;
+  const result = await ProductModel.create(product);
+  return result;
 };
-  export const ProductServices= {
-    createProductIntoDB,
-    getAllProductsFromDB,
-    getSingleProductFromDB,
-    updateProductFromDB
+const getAllProductsFromDB = async () => {
+  const result = await ProductModel.find();
+  return result;
+};
+
+const getSingleProductFromDB = async (id: string) => {
+  const result = await ProductModel.findOne({ id });
+  return result;
+};
+const updateProductFromDB = async (
+  productId: string,
+  updatedData: Partial<Product>
+) => {
+  // Check if the productId is a valid MongoDB ObjectID
+  if (!mongoose.Types.ObjectId.isValid(productId)) {
+    throw new Error("Invalid product ID");
   }
+
+  // Find and update the product
+  const updatedProduct = await ProductModel.findByIdAndUpdate(
+    productId,
+    updatedData,
+    { new: true, runValidators: true }
+  );
+
+  if (!updatedProduct) {
+    throw new Error("Product not found");
+  }
+
+  return updatedProduct;
+};
+const deleteProductFromDB = async (id: string) => {
+  const result = await ProductModel.deleteOne({ id });
+  return result;
+};
+export const ProductServices = {
+  createProductIntoDB,
+  getAllProductsFromDB,
+  getSingleProductFromDB,
+  updateProductFromDB,
+  deleteProductFromDB
+};
